ad320d7aae87a38305e2d4087ede7d204189b7d5
Jelle-Moody <jelle.moody@student.uva.nl>
2024-12-05 11:45:10 +0100

    Commit code fix
    Code works, deleted every file that doesn't do anything

--- src/OceanOptics/OceanOptics.py
+++ src/OceanOptics/OceanOptics.py
@@ -1,9 +1,23 @@
 import array
+import pathlib
+import platform
+import sys
 import time
 
-import plotext as plt
+import libusb
+import matplotlib.pyplot as plt
+import usb.backend.libusb1
 import usb.core
 import usb.util
+
+if usb.backend.libusb1.get_backend() is None:
+    if platform.system() == "Windows":
+        p_path = "x64" if sys.maxsize > 2**32 else "x86"
+        dll_path = (
+            pathlib.Path(libusb.__file__).parent
+            / rf"_platform\_windows\{p_path}\libusb-1.0.dll"
+        )
+        usb.backend.libusb1.get_backend(find_library=lambda x: dll_path)
 
 INT_TIME = 500_000
 
